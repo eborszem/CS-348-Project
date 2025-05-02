1b50a184d403b0dac5804b650a02826d524c4069
eborszem <eborszem@purdue.edu>
2025-05-02 02:17:25 -0400

    first commit, parts 1 and 2

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
+mysql = MySQL(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
